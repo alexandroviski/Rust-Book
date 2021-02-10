8212580b7e2bb73dbd27a32ee70671cb45cd9913
commitHarakiri <53496273+alexandroviski@users.noreply.github.com>
2021-02-10 17:11:34 -0400

    Update main.rs

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,42 +1,19 @@
-use ::std::error::Error;
 use std::env;
-use std::fs;
 use std::process;
 
-impl Config {
-    fn new(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
-
-        let query = args[1].clone();
-        let filename = args[2].clone();
-        Ok(Config { query, filename })
-    }
-}
+use minigrep::Config;
 
 fn main() {
-    // essa funcao nao aceita unicode
     let args: Vec<String> = env::args().collect();
 
     let config = Config::new(&args).unwrap_or_else(|err| {
-        println!("problem parsing arguments: {}", err);
+        eprintln!("problem parsing argument: {}", err);
         process::exit(1);
     });
-    if let Err(e) = run(config) {
-        println!("application error: {}", e);
+    
+
+    if let Err(e) = minigrep::run(config) {
+        eprintln!("application error: {}", e);
         process::exit(1);
     }
-}
-
-fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    let contents = fs::read_to_string(config.filename)?;
-
-    println!("with texto:\n{}", contents);
-    Ok(())
-}
-
-struct Config {
-    query: String,
-    filename: String,
 }
